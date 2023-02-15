04c8c7e745118fa9d07194bbf2bead23b708e4b4
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2023-02-14 22:38:41 -0300

    feat: add menubar component

---
+++ src/components/AppMenu.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Collapse,
+    Nav,
+    NavItem, 
+    NavLink,
+    NavbarToggler
+} from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+
+function AppMenu() {
+    const [open, setOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setOpen(!open)
+    }
+
+    return ( 
+        <Navbar color='light' light expand='md' >
+            <NavbarBrand tag={Link} to='/'>Minhas Séries</NavbarBrand>
+            <NavbarToggler onClick={toggleMenu} />
+            <Collapse isOpen={open} navbar>
+                <Nav className='ml-auto' navbar>
+                    <NavItem>
+                        <NavLink tag={Link} to='/generos'>Gêneros</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    )
+}
+
+export default AppMenu;
